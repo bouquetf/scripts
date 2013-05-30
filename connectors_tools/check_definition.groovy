44b533b2b11683f56cf379b9830dc05ebdf1e19f
bouquetf <hm0thx45>
2013-05-30 16:55:35 +0200

    Added check on widgets and pages

--- connectors_tools/check_definition.groovy
+++ connectors_tools/check_definition.groovy
@@ -31,13 +31,36 @@ def verify_definition(String definition_name) {
  * @param definition_name the path to the definition to check
  */
 def verify_properties(File propertiesFile, GPathResult xmlDef) {
+    println('*****' + propertiesFile.getName())
+
     PropertiesChecker propertiesChecker = new PropertiesChecker(propertiesFile)
 
+    verify_header(propertiesChecker, xmlDef)
+    verify_pages(xmlDef, propertiesChecker)
+    verify_widgets(xmlDef, propertiesChecker)
+}
+
+private void verify_header(PropertiesChecker propertiesChecker, GPathResult xmlDef) {
     propertiesChecker.checkProperty('ERROR', 'Category', xmlDef.category.@id.text() + '.category')
     propertiesChecker.checkProperty('ERROR', 'Connector label (connectorDefinitionLabel)', 'connectorDefinitionLabel')
     propertiesChecker.checkProperty('ERROR', 'Connector description (connectorDefinitionDescription)', 'connectorDefinitionDescription')
 }
 
+private void verify_pages(GPathResult xmlDef, PropertiesChecker propertiesChecker) {
+    for (def page : xmlDef.page) {
+        propertiesChecker.checkProperty('ERROR', 'Page label', page.@id.text() + '.pageTitle')
+        propertiesChecker.checkProperty('ERROR', 'Page Description', page.@id.text() + '.pageDescription')
+    }
+}
+
+private void verify_widgets(GPathResult xmlDef, PropertiesChecker propertiesChecker) {
+    for (widgetId in xmlDef.page.widget.@id) {
+        def widgetText = widgetId.text()
+        propertiesChecker.checkProperty('ERROR', 'Widget label ' + widgetText, widgetText + '.label')
+        propertiesChecker.checkPropertyEmpty('WARNING', 'Widget label ' + widgetText, widgetText + '.label')
+        propertiesChecker.checkProperty('ERROR', 'Widget description ' + widgetText, widgetText + '.description')
+    }
+}
 
 class PropertiesChecker {
     File propertiesFile
@@ -52,6 +75,12 @@ class PropertiesChecker {
     public void checkProperty(String level, String propertyName, String property) {
         if (properties.getProperty(property) == null) {
             println("$level $propertyName is not set for ${propertiesFile.getName()}")
+        }
+    }
+
+    def checkPropertyEmpty(String level, String propertyName, String property) {
+        if (properties.getProperty(property)?.isEmpty()) {
+            println("$level $propertyName is empty for ${propertiesFile.getName()}")
         }
     }
 }
